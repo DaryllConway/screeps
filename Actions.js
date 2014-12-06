7c78fd8ca0392b517215ef5e01a28ec5f1c3eaf1
Kelgors <matthieuboheas@gmail.com>
2014-12-06 06:00:16 +0100

    target for builders and calculate nearestSpawn from spawn list of creep room

--- Actions.js
+++ Actions.js
@@ -8,14 +8,18 @@ module.exports = (function () {
 
   Actions.harvest = function harvest(creep, index) {
     'use strict';
-    var nearestSpawn, site, sources;
+    var nearestSpawn, site, sources, spawns;
     if (creep.spawning) {
       return;
     }
 
     sources = creep.room.find(Game.SOURCES);
     site = sources[index % sources.length];
-    nearestSpawn = K.emptySpawn.findNearest(creep.pos);
+
+    spawns = new Collection(creep.room.find(Game.MY_SPAWNS));
+    nearestSpawn = spawns.filter(function (spawn) {
+      return spawn.energy < spawn.energyCapacity;
+    }).findNearest(creep.pos);
     if (!site || !nearestSpawn) {
       creep.moveTo(utils.getDefaultSpawn());
       return;
@@ -32,40 +36,45 @@ module.exports = (function () {
   };
   Actions.build = function build(creep, index) {
     'use strict';
-    var sites, site, nearestSpawn;
+    var sites, site, spawns, nearestSpawn;
     if (creep.spawning) {
       return;
     }
-    sites = creep.room.find(Game.CONSTRUCTION_SITES);
-    site  = sites[index % sites.length];
 
-    if (!site || creep.energy === 0) {
-      nearestSpawn = (K.fullSpawn.size() > 0 ? K.fullSpawn : K.emptySpawn).findNearest(creep.pos);
-      if (nearestSpawn.energy < nearestSpawn.energyCapacity / 6) {
-        nearestSpawn = (new Collection(creep.room.find(Game.MY_SPAWNS))).desc('energy').first();
-        if (nearestSpawn.energy < nearestSpawn.energyCapacity / 6)
+    if (!creep.target || creep.energy === 0) {
+      sites = creep.room.find(Game.CONSTRUCTION_SITES);
+      site  = sites[index % sites.length];
+
+      if (!site || creep.energy === 0) {
+        spawns = new Collection(creep.room.find(Game.MY_SPAWNS));
+        nearestSpawn = spawns.filter(function (spawn) {
+          return spawn.energy > spawn.energyCapacity / 3;
+        }).findNearest(creep.pos);
+        if (!nearestSpawn) {
           if (Game.flags.BuilderPoint) creep.moveTo(Game.flags.BuilderPoint);
           return;
-      }
-      if (nearestSpawn) {
-        if (!site) {
-          if (creep.energy) {
-            creep.moveTo(nearestSpawn);
-            creep.transferEnergy(nearestSpawn);
-          } else if (Game.flags.BuilderPoint) {
-            creep.moveTo(Game.flags.BuilderPoint);
+        }
+        if (nearestSpawn) {
+          if (!site) {
+            if (creep.energy) {
+              creep.moveTo(nearestSpawn);
+              creep.transferEnergy(nearestSpawn);
+            } else if (Game.flags.BuilderPoint) {
+              creep.moveTo(Game.flags.BuilderPoint);
+            }
           }
+          else if (creep.energy === 0 && nearestSpawn.energy >= creep.energyCapacity) {
+            creep.moveTo(nearestSpawn);
+            nearestSpawn.transferEnergy(creep);
+          }
+          return;
         }
-        else if (creep.energy === 0 && nearestSpawn.energy >= creep.energyCapacity) {
-          creep.moveTo(nearestSpawn);
-          nearestSpawn.transferEnergy(creep);
-        }
-        return;
+      } else {
+        creep.target = site;
       }
     }
-
-    creep.moveTo(site);
-    creep.build(site);
+    creep.moveTo(creep.target);
+    creep.build(creep.target);
   };
 
   return Actions;
