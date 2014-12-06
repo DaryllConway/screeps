39666329810032fd3dc387a8b7d5a8b6de254c78
Kelgors <matthieuboheas@gmail.com>
2014-12-06 06:01:24 +0100

    add update memory method to sanitize it

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 module.exports = (function () {
   'use strict';
+
   function getDefaultSpawn() {
     return Game.spawns[Memory.spawns.first()];
   }
@@ -37,22 +38,34 @@ module.exports = (function () {
     return other;
   }
 
+  function updateMemory() {
+    Memory.spawns = getObjectValues(Game.spawns).map(function (spawn) {
+      return spawn.name;
+    });
+    Object.keys(Memory.creeps).forEach(function (creepName) {
+      if (!Game.creeps[creepName]) {
+        delete Memory.creeps[creepName];
+      }
+    });
+  }
+
   var utils = {
-    "getDefaultSpawn": getDefaultSpawn,
-    "sumBodyParts": sumBodyParts,
-    "clone": clone,
+    "getDefaultSpawn" : getDefaultSpawn,
+    "sumBodyParts"    : sumBodyParts,
+    "clone"           : clone,
     "isLitteralObject": isLitteralObject,
-    "valuesOf": getObjectValues
+    "valuesOf"        : getObjectValues,
+    "updateMemory"    : updateMemory,
+    "bodyparts"       : {}
   };
 
-  utils.bodyparts = {};
-  utils.bodyparts[Game.MOVE] = 50;
-  utils.bodyparts[Game.WORK] = 20;
-  utils.bodyparts[Game.CARRY] = 50;
-  utils.bodyparts[Game.ATTACK] = 100;
+  utils.bodyparts[Game.MOVE]          = 50;
+  utils.bodyparts[Game.WORK]          = 20;
+  utils.bodyparts[Game.CARRY]         = 50;
+  utils.bodyparts[Game.ATTACK]        = 100;
   utils.bodyparts[Game.RANGED_ATTACK] = 150;
-  utils.bodyparts[Game.HEAL] = 200;
-  utils.bodyparts[Game.TOUGH] = 5;
+  utils.bodyparts[Game.HEAL]          = 200;
+  utils.bodyparts[Game.TOUGH]         = 5;
 
   return utils;
 })();
